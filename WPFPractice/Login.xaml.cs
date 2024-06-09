9d0e83ecbd0c75e5ff4156ddf3bb3ee5f96f1000
subramanis55 <subramanis6385@gmail.com>
2024-06-09 11:53:14 +0530

    first commit

---
+++ WPFPractice/Login.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFPractice
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+            demoname = "hello";
+
+
+
+        }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void SignInBtnClick(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(EmailTextBox.Text)&& !string.IsNullOrEmpty(PasswordTextBox.Password)) {
+                MessageBox.Show("Sign Successful");
+            }
+            else{
+                MessageBox.Show("Fill Details");
+                EmailTextBox.Clear();
+                PasswordTextBox.Clear();
+            }
+        }
+
+
+
+        public string demoname
+        {
+            get { return (string)GetValue(demonameProperty); }
+            set { SetValue(demonameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for demoname.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty demonameProperty =
+            DependencyProperty.Register("demoname", typeof(string), typeof(Login), new FrameworkPropertyMetadata(default(string),FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+    }
+}
---
+++ WPFPractice/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textEmailMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtEmail.Focus();
+        }
+
+        private void txtEmailTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+            {
+                txtEmail.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+               txtEmail.Visibility = Visibility.Visible;
+            }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && !string.IsNullOrEmpty(txtPassword.Password))
+            {
+                MessageBox.Show("Sign Successful");
+            }
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(txtPassword.Password) && txtPassword.Password.Length > 0)
+            {
+                txtPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                txtPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textPasswordDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPassword.Focus();
+        }
+    }
+}
